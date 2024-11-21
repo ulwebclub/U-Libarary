2ff995d663a6848ff922759755dc938f87e0d1b4
lightumcc <jessiezhu@createchstudio.com>
2024-11-21 15:59:18 +0800

    feat(backend): init function
    with swagger, log, cors, jwt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,43 @@
-import { Elysia } from "elysia";
+import {Elysia, t} from 'elysia';
+import {swagger} from '@elysiajs/swagger';
+import {cors} from "@elysiajs/cors";
+import {logger} from '@bogeychan/elysia-logger';
+import jwt from "@elysiajs/jwt";
+import dotenv from 'dotenv';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+dotenv.config()
+const WEB_URL = process.env.WEB_URL || 'http://localhost:4000';
+const JWT_SECRET = process.env.JWT_SECRET || "UL-WebClub"
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+
+new Elysia()
+    .use(swagger({
+        documentation: {
+            info: {
+                title: 'U-Library Backend API Documentation',
+                version: '1.0.0'
+            }
+        }
+    }))
+    .use(cors({
+        origin: WEB_URL,
+        credentials: true,
+        allowedHeaders: ['Content-Type', 'Origin', 'Cookie', 'Accept']
+    }))
+    .use(logger({
+        stream: process.stdout,
+        level: "error",
+    }))
+    .use(
+        jwt({
+            name: 'jwt',
+            secret: JWT_SECRET,
+        })
+    )
+    .get('/', () => {return  'Welcome to VGORC TM API Backend'})
+    .get('/ping', () => {return 'Pong!'})
+    .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+    `📚Welcome U-Library API System ${BASE_URL}`
 );
