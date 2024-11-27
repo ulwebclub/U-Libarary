0ec301bfb2648e6824a6ce8b63ce1d3f4a7dc368
RichardFu715 <87992235+RichardFu715@users.noreply.github.com>
2024-11-27 10:50:09 +0800

    Update page.tsx

--- frontend/app/borrow/page.tsx
+++ frontend/app/borrow/page.tsx
@@ -5,7 +5,7 @@ import {Table} from "@mui/material"
 export default function Page(){
     return (
         <div>
-            <p>To be developed</p>
+            <p>modified</p>
         </div>
     );
 }
