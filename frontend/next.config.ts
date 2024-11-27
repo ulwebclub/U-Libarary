97512b701db53bb2f1f199803a7288f3278e43a7
Iewnfod <shangzhiqiuyue@outlook.com>
2024-11-27 16:51:59 +0800

    feat(frontend): reverse proxy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    rewrites: async () => {
+        return [
+            {
+                source: '/api/:path',
+                destination: 'http://localhost:3000/:path'
+            }
+        ];
+    }
 };
 
 export default nextConfig;
