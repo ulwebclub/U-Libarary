5354bf1c1ccdd55423cef32583a457ef339822f5
RichardFu715 <87992235+RichardFu715@users.noreply.github.com>
2024-11-29 07:50:45 +0800

    feat(frontend): modified borrow table appearance

--- frontend/app/borrow/page.tsx
+++ frontend/app/borrow/page.tsx
@@ -3,18 +3,56 @@
 import * as React from "react";
 import {DataGrid, GridColDef} from "@mui/x-data-grid";
 import {Box, Button} from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 import {useState} from "react";
 import {InventoryObject, InventoryType} from "../../../common/Inventory";
 
 const cols = [
     {field: "id", headerName: "ID", width: 60},
-    {field: "title", headerName: "Title", width: 240},
-    {field: "author", headerName: "Author", width: 165},
+    {field: "title", headerName: "Title", width: 240, minWidth: 240, flex: 1},
+    {field: "author", headerName: "Author", width: 165, flex: 0},
     {field: "type", headerName: "Type", width: 60},
     {field: "isbn", headerName: "ISBN", width: 135},
-    {field: "borrowed", headerName: "Borrowed?", width: 90},
-    {field: "expectReturnTime", headerName: "Expected return time", width: 120},
-    {field: "reserved", headerName: "Reserved?", width: 90},
+    {field: "borrowed", headerName: "Borrowed?", width: 90,
+        renderCell: (params) => (
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center", // Center vertically
+                    justifyContent: "center", // Center horizontally
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                {params.value ? (
+                    <CheckIcon style={{ color: "green" }} />
+                ) : (
+                    <CloseIcon style={{ color: "red" }} />
+                )}
+            </Box>
+        ),
+    },
+    {field: "expectReturnTime", headerName: "Expected return time", width: 150},
+    {field: "reserved", headerName: "Reserved?", width: 90,
+        renderCell: (params) => (
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center", // Center vertically
+                    justifyContent: "center", // Center horizontally
+                    width: "100%",
+                    height: "100%",
+                }}
+            >
+                {params.value ? (
+                    <CheckIcon style={{ color: "green" }} />
+                ) : (
+                    <CloseIcon style={{ color: "red" }} />
+                )}
+            </Box>
+        ),
+    },
 ];
 
 /*
@@ -25,10 +63,22 @@ expectReturnTime: "2024/12/31", reserved: false}
 const paginationModel = { page: 0, pageSize: 10 };
 
 export default function Page(){
-    const [items, setItems] = useState<InventoryObject[]>([]);
+    const [items, setItems] = useState<InventoryObject[]>([{title: "Book 1", type: "Book", author: "Author 1", id: "1", isbn: "9788175257665", borrowed: true,
+        expectReturnTime: "2024/12/31", reserved: false}, {title: "Book 2", type: "Book", author: "Author 2", id: "2", isbn: "9788175257665", borrowed: true,
+        expectReturnTime: "2024/12/31", reserved: false}]);
 
     return (
-        <Box sx = {{width: '90%', minWidth: 375, justifyContent: 'center', alignItems: 'center'}}>
+        <Box sx = {{
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'sticky',
+            top: 0,
+            //zIndex: 'tooltip',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
             <DataGrid
                 columns = {cols}
                 rows = {items}
@@ -36,9 +86,10 @@ export default function Page(){
                 initialState = {{pagination: {paginationModel}}}
                 checkboxSelection = {true}
                 hideFooterSelectedRowCount = {false}
-                sx = {{width: '100%', minWidth: 375}}
+                sx = {{width: '90%', minWidth: 375}}
             />
-            <Box sx = {{width: '100%', justifyContent: 'left', alignItems: 'left', pt: 2}}>
+
+            <Box sx = {{width: '90%', display: "flex", justifyContent: 'flex-end', alignItems: 'right', pt: 1}}>
                 <Button
                     variant = "contained"
                 >
